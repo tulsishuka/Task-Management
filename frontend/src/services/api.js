0ecb3f41e3ea4136bf9fe6ffa305ebda15f21547
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-14 20:24:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://task-management-ntav.onrender.com/api',
+    // baseURL: 'http://localhost:5000/api',
+
 });
 
 API.interceptors.request.use(config => {
