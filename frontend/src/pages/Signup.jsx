3cecec84b6211262123e505e424dd45229b1b976
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-15 13:41:00 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -29,7 +29,7 @@ export default function Signup() {
         <input type="password" className="border p-2 mb-4 w-full" placeholder="Password" name="password" onChange={handleChange} />
         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded w-full">Signup</button>
         <p className="mt-4 text-center text-sm">
-          Don't have an account? <Link to="/Login" className="text-green-500 underline">Sign up</Link>
+          Don't have an account? <Link to="/Login" className="text-green-500 underline">Log In</Link>
         </p>
       </form>
     </div>
